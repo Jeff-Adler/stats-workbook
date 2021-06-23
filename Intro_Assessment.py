239a3629477f84926d2cbdb6634440dd07910ded
Jeff-Adler <Jeffm.adler@gmail.com>
2021-06-23 15:27:26 -0400

    first pass at calculating variance

--- Intro_Assessment.py
+++ Intro_Assessment.py
@@ -1,2 +1,25 @@
 # 1. Find the variance of B
 # B = { -5,3,12,190,-10 }
+
+list_of_values = [-5, 3, 12, 190, -10]
+
+def calculate_avg(list_of_values):
+  return sum(list_of_values) / len(list_of_values)
+
+avg_of_values = calculate_avg(list_of_values)
+
+def subtract_from_mean(n):
+  return n - avg_of_values
+
+def square_value(n):
+  return n * n
+
+differences_from_mean = [subtract_from_mean(n) for n in list_of_values]
+
+squared_differences = [square_value(n) for n in differences_from_mean]
+
+sum_of_squared_differences = sum(squared_differences)
+
+variance = sum_of_squared_differences / len(list_of_values)
+
+print(variance) 
---
+++ definitions.md
@@ -0,0 +1,3 @@
+## Statistics
+
+- In statistics, variance measures variability from the average or mean. It is calculated by taking the differences between each number in the data set and the mean, then squaring the differences to make them positive, and finally dividing the sum of the squares by the number of values in the data set.
