c84861ca9e7b35aaf0c9623509f820442edb5b3f
Jeff-Adler <Jeffm.adler@gmail.com>
2021-06-23 16:31:47 -0400

    got standard deviation

--- Intro_Assessment.py
+++ Intro_Assessment.py
@@ -1,29 +1,30 @@
 # 1. Find the variance of B
 # B = { -5,3,12,190,-10 }
+import math
 
 list_of_values = [-5, 3, 12, 190, -10]
-list_of_values_copy = list_of_values
+# list_of_values_copy = list_of_values
 
-def calculate_avg(list_of_values):
-  return sum(list_of_values) / len(list_of_values)
+# def calculate_avg(list_of_values):
+#   return sum(list_of_values) / len(list_of_values)
 
-avg_of_values = calculate_avg(list_of_values)
+# avg_of_values = calculate_avg(list_of_values)
 
-def subtract_from_mean(n):
-  return n - avg_of_values
+# def subtract_from_mean(n):
+#   return n - avg_of_values
 
-def square_value(n):
-  return n * n
+# def square_value(n):
+#   return n * n
 
-differences_from_mean = [subtract_from_mean(n) for n in list_of_values]
+# differences_from_mean = [subtract_from_mean(n) for n in list_of_values]
 
-squared_differences = [square_value(n) for n in differences_from_mean]
+# squared_differences = [square_value(n) for n in differences_from_mean]
 
-sum_of_squared_differences = sum(squared_differences)
+# sum_of_squared_differences = sum(squared_differences)
 
-variance = sum_of_squared_differences / len(list_of_values)
+# variance = sum_of_squared_differences / len(list_of_values)
 
-print("Variance from script: ", variance) 
+# print("Variance from script: ", variance) 
 
 def variance(data):
   # Number of observations
@@ -36,4 +37,6 @@ def variance(data):
   variance = sum(deviations) / n
   return variance
 
-print("Variance from function: ", variance(list_of_values_copy))
+print("Variance from function: ", variance(list_of_values))
+
+print("Square Root: ", math.sqrt(variance(list_of_values)))
